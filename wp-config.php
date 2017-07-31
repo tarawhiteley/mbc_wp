6a86a70f601eae431c24282069947096480a6ec1
Tara Whiteley <tarawhiteley@gmail.com>
2017-07-30 20:13:46 -0400

    commit before uploading site

--- wp-config.php
+++ wp-config.php
@@ -29,22 +29,34 @@ define('WP_CACHE', true); // Added by W3 Total Cache
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'mbc_wp');
+// Include local configuration
+if (file_exists(dirname(__FILE__) . '/local-config.php')) {
+	include(dirname(__FILE__) . '/local-config.php');
+}
 
-/** MySQL database username */
-define('DB_USER', 'root');
-
-/** MySQL database password */
-define('DB_PASSWORD', 'root');
-
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
+// Global DB config
+if (!defined('DB_NAME')) {
+	define('DB_NAME', 'midwestbusinessconsulting_wp');
+}
+if (!defined('DB_USER')) {
+	define('DB_USER', 'mbc');
+}
+if (!defined('DB_PASSWORD')) {
+	define('DB_PASSWORD', 'mbc2017');
+}
+if (!defined('DB_HOST')) {
+	define('DB_HOST', 'localhost');
+}
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8mb4');
+if (!defined('DB_CHARSET')) {
+	define('DB_CHARSET', 'utf8');
+}
 
 /** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+if (!defined('DB_COLLATE')) {
+	define('DB_COLLATE', '');
+}
 
 /**#@+
  * Authentication Unique Keys and Salts.
